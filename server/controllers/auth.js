da144196441db97d6c82eaec82e6432e5ba014a7
slayeroak <roakley1@villanova.edu>
2020-05-04 13:28:43 -0400

    user signup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = (req, res) => {
-    console.log('REQ BODY ON SIGNUP', req.body);
-    
-    res.json({
-        data: 'you hit signup endpoint yay from controllers'
+    // console.log('REQ BODY ON SIGNUP', req.body);
+    const { name, email, password } = req.body;
+
+    User.findOne({ email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is taken'
+            });
+        }
+    });
+
+    let newUser = new User({ name, email, password });
+
+    newUser.save((err, success) => {
+        if (err) {
+            console.log('SIGNUP ERROR', err);
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json({
+            message: 'Signup success! Please signin'
+        });
     });
 };
