6b18412f1958e8d6a41b686d29a2884b6ddd7f38
slayeroak <roakley1@villanova.edu>
2020-05-13 10:54:29 -0400

    .env fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ const App = () => {
     return (
         <Layout>
             <div className="col-md-6 offset-md-3 text-center">
-                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
-                <h2>MERN STACK</h2>
+                <h1 className="p-5">Slip Hook Project</h1>
+                <h2>Home Page material goes here</h2>
                 <hr />
                 <p className="lead">
+                    Project Features completed:
                     MERN stack login register system with account activation, forgot password, reset password, login
                     with facebook and google as well as private and protected routes for authenticated user and users
                     with the role of admin.
